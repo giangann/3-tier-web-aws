78fcc988def786150e89d3b5348ca77f78618cff
giangann <70595717+giangann@users.noreply.github.com>
2023-11-15 18:08:27 +0700

    initial

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cwefk7nyiwjt.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'root',
+    DB_PWD : 'password',
+    DB_DATABASE : 'webappdb'
+});
